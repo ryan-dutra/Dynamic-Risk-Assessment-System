0f254dd2ba916b97a936eaaafa8f649e78979ae8
ryan-dutra <rdutra.d.abreu@gmail.com>
2024-06-08 16:00:13 -0300

    Add scoring step

--- src/scoring.py
+++ src/scoring.py
@@ -1,25 +1,46 @@
-from flask import Flask, session, jsonify, request
-import pandas as pd
-import numpy as np
-import pickle
 import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-import json
+import sys
+import pickle
+import logging
+import pandas as pd
+from sklearn.metrics import f1_score
+
+from config import MODEL_PATH, TEST_DATA_PATH
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 
-
-#################Load config.json and get path variables
-with open('config.json','r') as f:
-    config = json.load(f) 
-
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-test_data_path = os.path.join(config['test_data_path']) 
-
-
-#################Function for model scoring
 def score_model():
-    #this function should take a trained model, load test data, and calculate an F1 score for the model relative to the test data
-    #it should write the result to the latestscore.txt file
+    """
+    Loads a trained model and the test data, and calculate an F1 score
+    for the model on the test data and saves the result to
+    the latestscore.txt file
+    """
+    logging.info("Loading testdata.csv")
+    test_df = pd.read_csv(os.path.join(TEST_DATA_PATH, 'testdata.csv'))
 
+    logging.info("Loading trained model")
+    model = pickle.load(
+        open(
+            os.path.join(
+                MODEL_PATH,
+                'trainedmodel.pkl'),
+            'rb'))
+
+    logging.info("Preparing test data")
+    y_true = test_df.pop('exited')
+    X_df = test_df.drop(['corporation'], axis=1)
+
+    logging.info("Predicting test data")
+    y_pred = model.predict(X_df)
+    f1 = f1_score(y_true, y_pred)
+    print(f"f1 score = {f1}")
+
+    logging.info("Saving scores to text file")
+    with open(os.path.join(MODEL_PATH, 'latestscore.txt'), 'w') as file:
+        file.write(f"f1 score = {f1}")
+
+
+if __name__ == '__main__':
+    logging.info("Running scoring.py")
+    score_model()
