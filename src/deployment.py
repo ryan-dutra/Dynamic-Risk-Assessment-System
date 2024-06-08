c6b8aa7bea85934214db8f542bcd7af6244cd24c
ryan-dutra <rdutra.d.abreu@gmail.com>
2024-06-08 16:02:18 -0300

    Add deployment step and generated files from execution

--- src/deployment.py
+++ src/deployment.py
@@ -1,27 +1,38 @@
-from flask import Flask, session, jsonify, request
-import pandas as pd
-import numpy as np
-import pickle
 import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
-import json
+import sys
+import shutil
+import logging
+
+from config import DATA_PATH, MODEL_PATH, PROD_DEPLOYMENT_PATH
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 
+def deploy_model():
+    """
+    Copy the latest model pickle file, the latestscore.txt value,
+    and the ingestfiles.txt file into the deployment directory
+    """
+    logging.info("Deploying trained model to production")
+    logging.info(
+        "Copying trainedmodel.pkl, ingestfiles.txt and latestscore.txt")
+    shutil.copy(
+        os.path.join(
+            DATA_PATH,
+            'ingestedfiles.txt'),
+        PROD_DEPLOYMENT_PATH)
+    shutil.copy(
+        os.path.join(
+            MODEL_PATH,
+            'trainedmodel.pkl'),
+        PROD_DEPLOYMENT_PATH)
+    shutil.copy(
+        os.path.join(
+            MODEL_PATH,
+            'latestscore.txt'),
+        PROD_DEPLOYMENT_PATH)
 
-##################Load config.json and correct path variable
-with open('config.json','r') as f:
-    config = json.load(f) 
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-prod_deployment_path = os.path.join(config['prod_deployment_path']) 
-
-
-####################function for deployment
-def store_model_into_pickle(model):
-    #copy the latest pickle file, the latestscore.txt value, and the ingestfiles.txt file into the deployment directory
-        
-        
-        
-
+if __name__ == '__main__':
+    logging.info("Running deployment.py")
+    deploy_model()
