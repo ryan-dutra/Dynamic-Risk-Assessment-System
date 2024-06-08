2f7bfe0566c07a37170a9700f46e02c075841708
ryan-dutra <rdutra.d.abreu@gmail.com>
2024-06-08 15:58:37 -0300

    Add training step

--- src/training.py
+++ src/training.py
@@ -1,32 +1,56 @@
-from flask import Flask, session, jsonify, request
-import pandas as pd
-import numpy as np
-import pickle
 import os
-from sklearn import metrics
-from sklearn.model_selection import train_test_split
+import sys
+import pickle
+import logging
+import pandas as pd
 from sklearn.linear_model import LogisticRegression
-import json
 
-###################Load config.json and get path variables
-with open('config.json','r') as f:
-    config = json.load(f) 
+from config import MODEL_PATH, DATA_PATH
 
-dataset_csv_path = os.path.join(config['output_folder_path']) 
-model_path = os.path.join(config['output_model_path']) 
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 
 
-#################Function for training the model
 def train_model():
-    
-    #use this logistic regression for training
-    LogisticRegression(C=1.0, class_weight=None, dual=False, fit_intercept=True,
-                    intercept_scaling=1, l1_ratio=None, max_iter=100,
-                    multi_class='warn', n_jobs=None, penalty='l2',
-                    random_state=0, solver='liblinear', tol=0.0001, verbose=0,
-                    warm_start=False)
-    
-    #fit the logistic regression to your data
-    
-    #write the trained model to your workspace in a file called trainedmodel.pkl
+    """
+    Train logistic regression model on ingested data and
+    and saves the model
+    """
+    logging.info("Loading and preparing finaldata.csv")
+    data_df = pd.read_csv(os.path.join(DATA_PATH, 'finaldata.csv'))
+    y_df = data_df.pop('exited')
+    X_df = data_df.drop(['corporation'], axis=1)
 
+    model = LogisticRegression(
+        C=1.0,
+        class_weight=None,
+        dual=False,
+        fit_intercept=True,
+        intercept_scaling=1,
+        l1_ratio=None,
+        max_iter=100,
+        multi_class='auto',
+        n_jobs=None,
+        penalty='l2',
+        random_state=0,
+        solver='liblinear',
+        tol=0.0001,
+        verbose=0,
+        warm_start=False)
+
+    logging.info("Training model")
+    model.fit(X_df, y_df)
+
+    logging.info("Saving trained model")
+    pickle.dump(
+        model,
+        open(
+            os.path.join(
+                MODEL_PATH,
+                'trainedmodel.pkl'),
+            'wb'))
+
+
+if __name__ == '__main__':
+    logging.info("Running training.py")
+    train_model()
+   
